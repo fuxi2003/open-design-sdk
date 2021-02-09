fe460f3149c276287b688bd515739429a4b023c3
jankuca <jan@jankuca.com>
2021-02-09 16:16:10 +0100

    add exports for documentation generation

---
+++ octopus-reader/src/docs.ts
@@ -0,0 +1,16 @@
+import type { IArtboard } from './types/artboard.iface'
+import type { IBitmap } from './types/bitmap.iface'
+import type { IEffects } from './types/effects.iface'
+import type { IFile } from './types/file.iface'
+import type { ILayer } from './types/layer.iface'
+import type { IPage } from './types/page.iface'
+import type { IText } from './types/text.iface'
+
+import type { ArtboardOctopusData } from './types/octopus.type'
+import type { ArtboardManifestData, ManifestData } from './types/manifest.type'
+
+export * from './index'
+
+export type { IArtboard, IBitmap, IEffects, IFile, ILayer, IPage, IText }
+
+export type { ArtboardManifestData, ArtboardOctopusData, ManifestData }
