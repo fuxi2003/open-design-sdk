3c187b999d237b86a8c62f52cab9ecf64f575984
jankuca <jan@jankuca.com>
2021-03-17 11:10:37 +0100

    expose interfaces from the rendering package

--- rendering/src/index.ts
+++ rendering/src/index.ts
@@ -1,3 +1,9 @@
 import { RenderingEngine } from './rendering-engine'
 
+import type { IRenderingArtboard } from './types/rendering-artboard.iface'
+import type { IRenderingDesign } from './types/rendering-design.iface'
+import type { IRenderingEngine } from './types/rendering-engine.iface'
+
 export { RenderingEngine }
+
+export type { IRenderingArtboard, IRenderingDesign, IRenderingEngine }
