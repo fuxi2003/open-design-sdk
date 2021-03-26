152d66a4cf0e89be153dfb60f6f63d35d4230c17
jankuca <jan@jankuca.com>
2021-03-26 15:31:05 +0100

    expose primitive types from rendering

--- rendering/src/index.ts
+++ rendering/src/index.ts
@@ -6,15 +6,23 @@ import type {
   LayerBounds,
   IRenderingArtboard,
 } from './types/rendering-artboard.iface'
+import type {
+  BlendingMode,
+  ClippingMode,
+  LayerType,
+} from './types/commands.type'
 import type { IRenderingDesign } from './types/rendering-design.iface'
 import type { IRenderingEngine } from './types/rendering-engine.iface'
 
 export { RenderingEngine }
 
 export type {
+  BlendingMode,
   Bounds,
+  ClippingMode,
   LayerAttributesConfig,
   LayerBounds,
+  LayerType,
   IRenderingArtboard,
   IRenderingDesign,
   IRenderingEngine,
